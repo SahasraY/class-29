2bcf693c6a367e9c0c94b65a1d398b1dc01035ba
SahasraY <yamini1013@gmail.com>
2020-12-15 15:54:49 +0530

    Modified

--- Slingshot.js
+++ Slingshot.js
@@ -9,6 +9,9 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+        this.sling1=loadImage("sprites/sling1.png")
+        this.sling2=loadImage("sprites/sling2.png")
+        this.sling3=loadImage("sprites/sling3.png")
     }
 
     fly(){
@@ -16,12 +19,35 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1,200,20)
+        image(this.sling2,170,20)
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
-        }
-    }
+
+
+          if(pointA.x<220){
+                strokeWeight(10);
+            stroke(84,39,15)
+            line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x+20, pointA.y, pointB.x+20, pointB.y-5);
+         
+        
+        image(this.sling3,pointA.x-30,pointA.y-20,20,20)
+       }
+       else{
+        strokeWeight(10);
+        stroke(84,39,15)
+        line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+        line(pointA.x+20, pointA.y, pointB.x+20, pointB.y-5);
+     
     
+        image(this.sling3,pointA.x+20,pointA.y-5,20,20)
+
+
+       }
+     }
+
+    
+}
 }
